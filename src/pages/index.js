1e31a459f6abb89cb7aacff08060e739653b0a6e
Cujba Mihai <stereo12@mail.ru>
2023-04-22 14:15:02 +0300

    Added spinner when search is processed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Upload } from 'antd';
-import { Input, Space } from 'antd';
+import { Input, Space, Spin } from 'antd';
 import QuizComponent from '../components/QuizComponent';
 import axios from 'axios';
 import { useState } from 'react';
 const { Search } = Input;
+
 const props = {
   action: '/api/upload',
   onChange({ file, fileList }) {
@@ -14,32 +15,43 @@ const props = {
   },
   defaultFileList: [],
 };
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
   const [results, setResults ] = useState([]);
+
   const onSearch = async (value) => {
+    setLoading(true)
     const response = await axios.get(`/api/search?question=${value}`, {
       method: 'GET',
     })
 
     setResults(response.data.results)
+    setLoading(false)
+
   }
 
 
  return (
     <><Space>
      <Upload {...props}>
-       <Button icon={<UploadOutlined />}>Upload</Button>
+       <Button icon={<UploadOutlined />}>Incarca HTML File-ul</Button>
      </Upload>
 
      <Search
-       placeholder="input search text"
+       placeholder="Cauta dupa intrebare"
        allowClear
        enterButton="Search"
        size="large"
        onSearch={onSearch} />
-   </Space><Space>
+   </Space>
+   
+    <Space>
+    <Spin spinning={loading}>
        <QuizComponent data={results} />
-     </Space></>
+    </Spin>
+    </Space>
+    </>
   )
 };
 export default App;
