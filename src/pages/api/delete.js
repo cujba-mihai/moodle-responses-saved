3511e7cc1a0588aeae27cdbf3d2fd65edf4f6ff9
Cujba Mihai <stereo12@mail.ru>
2023-04-23 13:52:08 +0300

    added diacritics ignore

---
+++ src/pages/api/delete.js
@@ -0,0 +1,19 @@
+import nextConnect from 'next-connect';
+import { getMongoDb } from '../../utils/db';
+import _ from 'lodash';
+
+const handler = nextConnect()
+
+handler.delete(async (req, res) => {
+    // const db = await getMongoDb();
+    // const collection = db.collection('questions');
+
+    // const results = await collection.deleteMany({ type: 'truefalse' });
+
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-type', 'application-json');
+    // res.end(JSON.stringify({results}, null, 2))
+})
+
+export default handler;
--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -3,11 +3,28 @@ import { getMongoDb } from '../../utils/db';
 import _ from 'lodash';
 
 const handler = nextConnect()
+const regex = /a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi;
 
 handler.get(async (req, res) => {
     const { question } = req.query;
+
+    const replacedText = question.replace(regex, function(match) {
+    switch(match.toLowerCase()) {
+        case 'a':
+        return '[aăâ]';
+        case 'i':
+        return '[iîâ]';
+        case 's':
+        return '[sș]';
+        case 't':
+        return '[tț]';
+        default:
+        return match;
+    }
+    });
+
     const db = await getMongoDb();
-    const regexQuery = { question: { $regex: question, $options: "i" } };
+    const regexQuery = { question: { $regex: replacedText, $options: "i" } };
     const results = await db.collection('questions').find(regexQuery).toArray();
     const sortedResults = _.orderBy(results, [(item) => (item.maxScore === 0 ? 0 : item.score / item.maxScore)], ['desc']);
 
