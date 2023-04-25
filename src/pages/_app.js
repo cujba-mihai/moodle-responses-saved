2921b2dcf75d33c2ccf01e531f20988d35951feb
Cujba Mihai <stereo12@mail.ru>
2023-04-25 11:38:55 +0300

    styles antd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import "/styles/antd.min.css"; // add this
+import "@/styles/antd.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
