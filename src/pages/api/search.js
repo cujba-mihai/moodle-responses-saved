3f2687b8b3471abae65a8e869f47f51d2bce8b74
Cujba Mihai <stereo12@mail.ru>
2023-04-27 13:04:13 +0300

    ref: search improvement

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -4,44 +4,20 @@ import _ from 'lodash';
 
 const handler = nextConnect()
 
-const regex = /[aist]/gi;
-const diacriticsMap = {
-    a: '[aăâ]',
-    i: '[iîâ]',
-    s: '[sș]',
-    t: '[tț]',
-};
-
-const escapeRegex = (str) => {
-    return str.replace(/[-[\]{}()*+?.,\\/^$|#\s]/g, '\\$&');
-};
-
 function removeDiacritics(text) {
-  const romanianDiacritics = /ă|â|î|ș|ț|Ă|Â|Î|Ș|Ț/g;
-  return text.replace(romanianDiacritics, '');
+  const romanianDiacritics = /ă|â|î|ș|ț|Ă|Â|Î|Ș|Ț|\W/g;
+  return text.replace(romanianDiacritics, '.');
 }
 
 
 handler.get(async (req, res) => {
     const { question, offset, limit } = req.query;
     const normalizedText = removeDiacritics(question.normalize().trim());
-    const words = normalizedText.split(/\s+/);
-
-    const regexQueries = words.map((word) => {
-        const escapedWord = escapeRegex(word);
-        const replacedWord = escapedWord.replace(/[aist]/gi, (match) => {
-            const lowerCaseMatch = match.toLowerCase();
-            return diacriticsMap[lowerCaseMatch] || match;
-        });
-        return { question: { $regex: replacedWord, $options: 'i' } };
-    });
-    const query = { $and: regexQueries };
 
     const db = await getMongoDb();
-    const regexQuery = query;
-    const totalResults = await db.collection('questions').countDocuments(regexQuery);
+    const totalResults = await db.collection('questions').countDocuments({ question: { $regex: normalizedText, $options: 'i' }});
     const results = await db.collection('questions')
-        .find(query)
+        .find({ question: { $regex: normalizedText, $options: 'i' }})
         .sort({ scoreRatio: -1 }) // Sort in descending order by scoreRatio
         .skip(parseInt(offset))
         .limit(parseInt(limit))
